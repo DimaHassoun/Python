d614e4b7ad63b661fb0dc8abaf35e1cc563e79e7
Mayson-S <selawemeson@gmail.com>
2025-12-14 17:35:01 +0200

    Add files via upload

--- python_exe1/src/view/BackgroundPanel.java
+++ python_exe1/src/view/BackgroundPanel.java
@@ -5,10 +5,10 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
-
+// A custom JPanel that displays a background image stretched to fit the panel.
 public class BackgroundPanel extends JPanel {
     private BufferedImage image;
-
+    // Constructs a BackgroundPanel with the specified image.
     public BackgroundPanel(String imagePath) {
         try {
             image = ImageIO.read(new File(imagePath));
@@ -17,7 +17,7 @@ public class BackgroundPanel extends JPanel {
         }
         setLayout(null); 
     }
-
+    // Paints the background image, scaling it to fit the panel's size.
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
