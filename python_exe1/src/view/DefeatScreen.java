f85a460282e46ce40e16056e1a94a7ffffe5c62b
eslamkelany <122433939+eslamkelany@users.noreply.github.com>
2026-01-07 21:50:37 +0200

    Add player parameters to DefeatScreen constructor

--- python_exe1/src/view/DefeatScreen.java
+++ python_exe1/src/view/DefeatScreen.java
@@ -6,8 +6,8 @@ import javax.swing.JFrame;
 
 public class DefeatScreen extends ResultScreen {
 
-    public DefeatScreen(int finalScore, JFrame previousWindow) {
-        super(finalScore, previousWindow);
+    public DefeatScreen(int finalScore, JFrame previousWindow, String player1, String player2) {
+        super(finalScore, previousWindow, player1, player2);
     }
 
     protected String getWindowTitle() {
@@ -35,4 +35,3 @@ public class DefeatScreen extends ResultScreen {
     }
 
 }
-
