73893f7907ccf3ef2472506d70e4e027ed511e0c
eslamkelany <122433939+eslamkelany@users.noreply.github.com>
2026-01-09 17:23:37 +0200

    Refactor ResultScreen class for improved structure

--- python_exe1/src/view/ResultScreen.java
+++ python_exe1/src/view/ResultScreen.java
@@ -135,9 +135,9 @@ public abstract class ResultScreen extends JFrame {
         btnNewGame.addActionListener((ActionEvent e) -> {
             dispose();
             if (previousWindow != null) previousWindow.dispose();
-            // فتح NewGameScreen مع نفس الأسماء
+            // Open NewGameScreen with the same names
             NewGameScreen newGameScreen = new NewGameScreen();
-            newGameScreen.setPlayerNames(player1, player2); // تحتاج دالة لتعيين الحقول
+            newGameScreen.setPlayerNames(player1, player2); 
             newGameScreen.setVisible(true);
         });
 
